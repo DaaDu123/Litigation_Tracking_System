1f6f3c9c1734d7c586c0ac9b5afb45bb06b8b6ce
DaaDu123 <saadmuhammad19115@gmail.com>
2026-07-26 23:27:15 +0500

    appdbcontext migration

--- LTSBackend/Migrations/20260726112005_Court_IsActive_Case_NullableFKs.Designer.cs
+++ LTSBackend/Migrations/20260726112005_Court_IsActive_Case_NullableFKs.Designer.cs
@@ -4,6 +4,7 @@ using LTSBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LTSBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260726112005_Court_IsActive_Case_NullableFKs")]
+    partial class Court_IsActive_Case_NullableFKs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
